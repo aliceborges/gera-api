d9675b17bd2b5ae01bc98a8b732d41e6aecc20c0
Alice Santos <alice.santos@contaazul.com>
2021-03-22 15:25:10 -0300

    build: commit inicial

---
+++ src/test/java/com/sumarizer/sumarizer/SumarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sumarizer.sumarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
